05c49a425c2cf04d9343c2cb493e748c85709a22
Sami Omidi <samioimidi@hotmail.com>
2026-05-12 19:10:41 +0330

    fix theme bugs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,70 +1,12 @@
-import type { Config } from "@docusaurus/types";
-import { themes as prismThemes } from "prism-react-renderer";
-
-const config: Config = {
-  title: "EARTH REBORN",
-  tagline: "Persistent MMO Economy Civilization",
-  favicon: "img/favicon.ico",
-
-  url: "https://docs.earthreborn.com",
-  baseUrl: "/",
-
-  organizationName: "earth-reborn",
-  projectName: "earth-reborn-docs",
-
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
-
-  presets: [
-    [
-      "classic",
-      {
-        docs: {
-        routeBasePath: '/docs',
-          sidebarPath: require.resolve("./sidebars.ts"),
-        },
-        blog: false,
-        theme: {
-          customCss: "./src/css/custom.css",
-        },
-      },
-    ],
-  ],
-
+export default {
   themeConfig: {
-    image: "img/docusaurus-social-card.jpg",
-    navbar: {
-      title: "EARTH REBORN",
-      items: [
-        {
-          type: "docSidebar",
-          sidebarId: "sidebar",
-          docsPluginId: "default",
-          position: "left",
-          label: "Documentation",
-        },
-        {
-          href: "https://github.com/Samiomidi/earth-reborn-docs",
-          label: "GitHub",
-          position: "right",
-        },
-      ],
-    },
     colorMode: {
       defaultMode: "dark",
       disableSwitch: false,
       respectPrefersColorScheme: true,
     },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
   },
-};
 
-export default config;
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+};
